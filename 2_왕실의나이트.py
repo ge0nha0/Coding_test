3dc9c67fa35b0a6b19f7c903a803b9e000159526
ge0nha0 <gunha8147@kookmin.ac.kr>
2024-08-04 12:17:24 +0900

    ecotest

--- 2_왕실의나이트.py
+++ 2_왕실의나이트.py
@@ -2,26 +2,26 @@
 # input a1 -> output 2
 ## 나이트의 8가지 경로를 하나씩 확인 + 8가지 방향에 대한 방향 벡터 정의 
 
-# now = input()
-# row, col = now[1], now[0]
+now = input()
+row, col = now[1], now[0]
 
-# col_lst = [0,'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-# col = col_lst.index(col)
-# row = int(row)
+col_lst = [0,'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+col = col_lst.index(col)
+row = int(row)
 
-# dx = [2, 2, -2, -2, 1, -1, 1, -1]
-# dy = [1, -1, 1, -1, 2, 2, -2, -2]
+dx = [2, 2, -2, -2, 1, -1, 1, -1]
+dy = [1, -1, 1, -1, 2, 2, -2, -2]
 
-# result = 0
+result = 0
 
-# for i in range(len(dx)):
-#     x = row + dx[i]
-#     y = col + dy[i] 
+for i in range(len(dx)):
+    x = row + dx[i]
+    y = col + dy[i] 
 
-#     if 1 <= x <= 8 and 1 <= y <= 8:
-#         result += 1 
+    if 1 <= x <= 8 and 1 <= y <= 8:
+        result += 1 
 
-# print(result)
+print(result)
 
 # 정답 풀이
 input_data = input()
